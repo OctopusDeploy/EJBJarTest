454cfec8849168385aa274fecce39826a17031d3
Matthew Casperson <matthewcasperson@gmail.com>
2017-08-02 06:14:31 +1000

    For now just rename the jar to a zip

--- pack.sh
+++ pack.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 pushd target
-/mnt/c/Apps/OctopusTools.4.18.0.ubuntu.16.04-x64/Octo pack --include ejbtest.jar --id=EJBTest --version=0.0.0.1 --format=zip --overwrite
+#/mnt/c/Apps/OctopusTools.4.18.0.ubuntu.16.04-x64/Octo pack --include ejbtest.jar --id=EJBTest --version=0.0.0.1 --format=zip --overwrite
+rm EJBTest.0.0.0.1.zip
+cp ejbtest.jar EJBTest.0.0.0.1.zip
 popd
