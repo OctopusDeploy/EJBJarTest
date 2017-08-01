6c26d2ded127522af0fef51cb02d2fc92b516053
Matthew Casperson <matthewcasperson@gmail.com>
2017-08-02 05:59:17 +1000

    Changed package type and made logging to std out

--- src/main/java/com/octopus/example/EJBTest.java
+++ src/main/java/com/octopus/example/EJBTest.java
@@ -3,19 +3,14 @@ package com.octopus.example;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  * An example of an EJB class
  */
 @Singleton
 @Startup
 public class EJBTest {
-    private static final Logger LOGGER = Logger.getLogger(EJBTest.class.getName());
-
     @PostConstruct
     public void onStart() {
-        LOGGER.log(Level.INFO, "EJB has started!");
+        System.out.println("EJB has started!");
     }
 }
